ae4730cfd5b3c60201deb807dc3c5fb16892cfbb
WhereIsLijah <114761023+WhereIsLijah@users.noreply.github.com>
2024-03-12 07:12:48 +0000

    Form creation

---
+++ appventure/accesspoint/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    email = forms.EmailField(max_length=255, required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email Address'}))
+    password = forms.CharField(max_length=128, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=128, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+    terms_condition = forms.CharField(required=True, widget=forms.CheckboxInput(attrs={'default': True}))
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
