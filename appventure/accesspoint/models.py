5beb81c8eaae09700444eda5bd66ee6ea7766f6b
WhereIsLijah <114761023+WhereIsLijah@users.noreply.github.com>
2024-03-13 04:14:25 +0000

    Model update

--- appventure/accesspoint/models.py
+++ appventure/accesspoint/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=128)
+    password2 = models.CharField(max_length=128)
+
+    
